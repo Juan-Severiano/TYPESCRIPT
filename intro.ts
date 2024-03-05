faf1f9a8dcfcc254398ff23b70a90b3d2b545e31
Francisco Juan <juansoussev@gmail.com>
2024-03-05 19:14:24 -0300

    [config] add tsconfig file

--- intro.ts
+++ intro.ts
@@ -1,2 +1,20 @@
 let nome: string = 'Juan';
-nome = 'Severiano';
+nome = 'Hemerson';
+
+// NUMBER
+let numero: number = 23;
+
+// STRING
+const sobrenome: string = 'Vida loka'
+
+const nomeCompleto  = fullName(nome, sobrenome);
+console.log(nomeCompleto)
+console.log(numero)
+
+// Inference
+let turma = 'FRONT-END'
+console.log(typeof turma)
+
+function fullName(firstName: string, lastName: string): string {
+  return `${firstName} ${lastName}`
+}
